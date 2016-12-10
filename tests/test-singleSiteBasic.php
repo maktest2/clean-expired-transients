8bd20e0aa833e1099f16f1854240205f1c2b89e8
milan <milan@srpski.biz>
2016-12-10 22:12:30 +0000

    Add test with cleaning.

--- tests/test-singleSiteBasic.php
+++ tests/test-singleSiteBasic.php
@@ -37,4 +37,35 @@ class SingleSiteBasic extends WP_UnitTestCase {
 		$fresh_get_key1 = get_transient( 'key1' );
 		$this->assertFalse( $fresh_get_key1 );
 	}
+
+	/**
+	 * Test when there is cleaning.
+	 */
+	public function test_with_cleaning() {
+		// Test setting of transient
+		$set_key2 = set_transient( 'key2', 'value2', 5 );
+		$this->assertTrue( $set_key2 );
+
+		// Test retrieving of transient
+		$get_key2 = get_transient( 'key2' );
+		$this->assertEquals( $get_key2, 'value2' );
+
+		// Sleep for half a minute
+		sleep( 30 );
+
+		// Do cleaning
+		Clean_Expired_Transients::clean();
+
+		// Test direct retrieval of transient value
+		$raw_key2 = get_option( '_transient_key2' );
+		$this->assertFalse( $raw_key2 );
+
+		// Test direct retrieval of transient timeout is less than current time`
+		$raw_key2_timeout = get_option( '_transient_timeout_key2' );
+		$this->assertFalse( $raw_key2_timeout );
+
+		// Test retrieving of expired transient
+		$fresh_get_key2 = get_transient( 'key2' );
+		$this->assertFalse( $fresh_get_key2 );
+	}
 }
