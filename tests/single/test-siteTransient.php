2dbe2f5d8d2493334a45f4b15c6dc6a168f53911
milan <milan@srpski.biz>
2016-12-13 17:48:56 +0000

    Test with multisite option.

--- tests/single/test-siteTransient.php
+++ tests/single/test-siteTransient.php
@@ -17,10 +17,10 @@ class Tests_Single_SiteTransient extends WP_UnitTestCase {
  		$this->assertTrue( set_site_transient( 'key1', 'value1', 5 ) );
 
  		// Direct retrieval of transient value should return setted value
- 		$this->assertEquals( get_option( '_site_transient_key1' ), 'value1' );
+ 		$this->assertEquals( get_site_option( '_site_transient_key1' ), 'value1' );
 
  		// Direct retrieval of transient timeout should be integer and less than current time`
- 		$raw_key1_timeout_before_sleep = get_option( '_site_transient_timeout_key1' );
+ 		$raw_key1_timeout_before_sleep = get_site_option( '_site_transient_timeout_key1' );
  		$this->assertTrue( is_int( $raw_key1_timeout_before_sleep ) );
  		$this->assertGreaterThan( time(), $raw_key1_timeout_before_sleep );
 
@@ -31,10 +31,10 @@ class Tests_Single_SiteTransient extends WP_UnitTestCase {
  		sleep( 2 * MINUTE_IN_SECONDS );
 
  		// Direct retrieval of transient value should return setted value
- 		$this->assertEquals( get_option( '_site_transient_key1' ), 'value1' );
+ 		$this->assertEquals( get_site_option( '_site_transient_key1' ), 'value1' );
 
  		// Direct retrieval of transient timeout should be integer and less than current time`
- 		$raw_key1_timeout_after_sleep = get_option( '_site_transient_timeout_key1' );
+ 		$raw_key1_timeout_after_sleep = get_site_option( '_site_transient_timeout_key1' );
  		$this->assertTrue( is_int( $raw_key1_timeout_after_sleep ) );
  		$this->assertLessThan( time(), $raw_key1_timeout_after_sleep );
 
@@ -42,10 +42,10 @@ class Tests_Single_SiteTransient extends WP_UnitTestCase {
  		$this->assertFalse( get_site_transient( 'key1' ) );
 
  		// Direct retrieval of expired transient value should be false
- 		$this->assertFalse( get_option( '_site_transient_key1' ) );
+ 		$this->assertFalse( get_site_option( '_site_transient_key1' ) );
 
  		// Direct retrieval of expired transient timeout should be false
- 		$this->assertFalse( get_option( '_site_transient_timeout_key1' ) );
+ 		$this->assertFalse( get_site_option( '_site_transient_timeout_key1' ) );
  	}
 
  	/**
@@ -56,10 +56,10 @@ class Tests_Single_SiteTransient extends WP_UnitTestCase {
  		$this->assertTrue( set_site_transient( 'key2', 'value2', 5 ) );
 
  		// Direct retrieval of transient value should return setted value
- 		$this->assertEquals( get_option( '_site_transient_key2' ), 'value2' );
+ 		$this->assertEquals( get_site_option( '_site_transient_key2' ), 'value2' );
 
  		// Direct retrieval of transient timeout should be integer and less than current time`
- 		$raw_key2_timeout_before_sleep = get_option( '_site_transient_timeout_key2' );
+ 		$raw_key2_timeout_before_sleep = get_site_option( '_site_transient_timeout_key2' );
  		$this->assertTrue( is_int( $raw_key2_timeout_before_sleep ) );
  		$this->assertGreaterThan( time(), $raw_key2_timeout_before_sleep );
 
@@ -73,18 +73,18 @@ class Tests_Single_SiteTransient extends WP_UnitTestCase {
  		Clean_Expired_Transients::clean();
 
  		// Direct retrieval of expired and cleaned transient value should be false
- 		$this->assertFalse( get_option( '_site_transient_key2' ) );
+ 		$this->assertFalse( get_site_option( '_site_transient_key2' ) );
 
  		// Direct retrieval of expired and cleaned transient timeout should be false
- 		$this->assertFalse( get_option( '_site_transient_timeout_key2' ) );
+ 		$this->assertFalse( get_site_option( '_site_transient_timeout_key2' ) );
 
  		// Getting of expired transient and cleaned should be false
  		$this->assertFalse( get_site_transient( 'key2' ) );
 
  		// Direct retrieval of expired and cleaned transient value should be false
- 		$this->assertFalse( get_option( '_site_transient_key2' ) );
+ 		$this->assertFalse( get_site_option( '_site_transient_key2' ) );
 
  		// Direct retrieval of expired and cleaned transient timeout should be false
- 		$this->assertFalse( get_option( '_site_transient_timeout_key2' ) );
+ 		$this->assertFalse( get_site_option( '_site_transient_timeout_key2' ) );
  	}
 }
